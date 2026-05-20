0c6d4026d27df532ec13d309d8a6bbf79b45159d
MoatazHussein <Moatazhussein432@gmail.com>
2026-05-21 00:10:59 +0300

    Update TapCheckoutOptions default property values

--- src/Talaby.Application/Common/TapCheckoutOptions.cs
+++ src/Talaby.Application/Common/TapCheckoutOptions.cs
@@ -4,11 +4,11 @@ public sealed class TapCheckoutOptions
 {
     public const string SectionName = "Tap";
 
-    public string SourceId { get; init; } = "src_all";
+    public string SourceId { get; init; } = default!;
 
     public string FrontendBaseUrl { get; init; } = default!;
 
     public string ApiPublicBaseUrl { get; init; } = default!;
 
-    public decimal CommissionPercentage { get; init; } = 2.5m;
+    public decimal CommissionPercentage { get; init; }
 }
